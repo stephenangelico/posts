1ff303ebe487541ba6196803b9d1486170fb9f77
Stephen Angelico <raaff35lightning@gmail.com>
2017-07-23 00:25:58 +1000

    Add title filtering

--- posts/api.py
+++ posts/api.py
@@ -12,9 +12,16 @@ from .database import session
 @decorators.accept("application/json")
 def posts_get():
 	""" Get a list of posts """
-	# Get the posts from the database
-	posts = session.query(models.Post).order_by(models.Post.id)
+	# Get the querystring arguments
+	title_like = request.args.get("title_like")
 	
+	# Get and filter the posts from the database
+	posts = session.query(models.Post)
+	if title_like:
+		posts = posts.filter(models.Post.title.contains(title_like))
+	posts = posts.order_by(models.Post.id)
+	
+	# Convert the posts to JSOM and return a response
 	data = json.dumps([post.as_dictionary() for post in posts])
 	return Response(data, 200, mimetype="application/json")
 
